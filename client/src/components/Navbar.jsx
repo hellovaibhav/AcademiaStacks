7debc3c0458d2f106834cceefdc8738ea31e82af
Saquib1973 <saquib.21ug2015@iiitranchi.ac.in>
2023-03-13 16:20:00 +0530

    added transitions on navbar , register page and login page 1.0.1

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,28 +1,142 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 import { FaUserAlt } from "react-icons/fa";
+import { motion } from "framer-motion";
 const Navbar = () => {
   return (
-    <div className="Navbar flex items-center justify-between">
+    <div className="Navbar flex items-center justify-between overflow-hidden">
       <div className="w-[40vw] z-0 pl-20 pt-4 ">
-        <Link to="/" className="text-4xl ">
-          LOGO
-        </Link>
+        <motion.p
+          initial={{ x: 100, opacity: 0 }}
+          whileInView={{
+            x: 0,
+            opacity: 1,
+            transition: {
+              duration: 1,
+              delay: 0,
+              type: "spring",
+              stiffness: 200,
+            },
+          }}
+        >
+          <Link to="/" className="text-4xl ">
+            LOGO
+          </Link>
+        </motion.p>
       </div>
-      <div className="w-[60vw] text-white h-[3.8rem] flex items-center justify-around rounded-bl-[5rem] pl-10 before:content-['']-bg-color[redx] bg-[#22A39F]">
-        <Link to="/material" className="text-2xl">
-          Material
-        </Link>
-        <Link to="/about" className="text-2xl">
-          About Us
-        </Link>
-        <Link to="/feedback" className="text-2xl">
-          Feedback
-        </Link>
-        <Link to="/login" className=" text-lg italic">
-          <FaUserAlt />
-        </Link>
-      </div>
+      <motion.div className="w-[60vw] text-white h-[3.8rem] flex items-center justify-around rounded-bl-[5rem] pl-10  bg-[#22A39F]"
+      initial={{y:-10 , x:10}}
+      animate={{y:0 , x:0 ,transition:{duration:1}}}
+      >
+        <motion.p
+          initial={{ y: -50, opacity: 0 }}
+          animate={{
+            y: 0,
+            opacity: 1,
+            transition: {
+              duration: 1,
+              delay: 0,
+              type: "spring",
+              stiffness: 200,
+            },
+          }}
+          // whileHover={{ scale: 1.2, transition: { duration: 0.4 } }}
+          whileTap={{scale:0.9 , transition: { duration: 0.3 }}}
+        >
+          <NavLink
+            to="/material"
+            className="dad text-2xl  hover:shadow-stone-600 hover:rounded-2xl hover:shadow-[7px_-1px_41px_-9px_rgba(0,0,0,0.75)] duration-300 px-4 py-1 "
+          >
+            Material
+          </NavLink>
+        </motion.p>
+        <motion.p
+          initial={{ y: -50, opacity: 0 }}
+          animate={{
+            y: 0,
+            opacity: 1,
+            transition: {
+              duration: 1,
+              delay: 0,
+              type: "spring",
+              stiffness: 200,
+            },
+          }}
+          // whileHover={{ scale: 1.2, transition: { duration: 0.4 } }}
+          whileTap={{scale:0.9 , transition: { duration: 0.3 }}}
+
+        >
+          <NavLink
+            to="/about"
+            className="dad text-2xl  hover:shadow-stone-600 hover:rounded-2xl hover:shadow-[7px_-1px_41px_-9px_rgba(0,0,0,0.75)] duration-300 px-4 py-1 "
+          >
+            About Us
+          </NavLink>
+        </motion.p>
+        <motion.p
+          initial={{ y: -50, opacity: 0 }}
+          animate={{
+            y: 0,
+            opacity: 1,
+            transition: {
+              duration: 1,
+              delay: 0,
+              type: "spring",
+              stiffness: 200,
+            },
+          }}
+          // whileHover={{ scale: 1.2, transition: { duration: 0.4 } }}
+          whileTap={{scale:0.9 , transition: { duration: 0.3 }}}
+
+        >
+          <NavLink
+            to="/feedback"
+            className="dad text-2xl  hover:shadow-stone-600 hover:rounded-2xl hover:shadow-[7px_-1px_41px_-9px_rgba(0,0,0,0.75)] duration-300 px-4 py-1 "
+          >
+            Feedback
+          </NavLink>
+        </motion.p>
+        <motion.p
+          initial={{ y: -50, opacity: 0 }}
+          animate={{
+            y: 0,
+            opacity: 1,
+            transition: {
+              duration: 1,
+              delay: 0,
+              type: "spring",
+              stiffness: 200,
+            },
+          }}
+          // whileHover={{ scale: 1.2, transition: { duration: 0.4 } }}
+          whileTap={{scale:0.9 , transition: { duration: 0.3 }}}
+
+        ></motion.p>
+
+        <motion.p
+          initial={{ y: -50, opacity: 0 }}
+          animate={{
+            y: 0,
+            opacity: 1,
+            transition: {
+              duration: 1,
+              delay: 0,
+              type: "spring",
+              stiffness: 200,
+            },
+          }}
+          // whileHover={{ scale: 1.2, transition: { duration: 0.4 } }}
+          whileTap={{scale:0.9 , transition: { duration: 0.3 }}}
+
+        >
+          <NavLink
+            to="/login"
+            className=" hover:shadow-stone-600 hover:rounded-2xl  duration-300 px-4 py-1 text-xl italic"
+          >
+            <FaUserAlt />
+          </NavLink>
+        </motion.p>
+      </motion.div>
     </div>
   );
 };
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className="min-h-[86.2vh] flex items-center justify-center ">About</div>
+    <div className="min-h-[85.4vh] flex items-center justify-center ">About</div>
   )
 }
 
