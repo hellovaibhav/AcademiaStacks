acd55d69b363905be23950100f98dae56207b83e
Vaibhav <vermavaibhav2005@gmail.com>
2023-03-13 15:36:18 +0530

    added mongodb schema 1.0

---
+++ api/models/Material.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const materialSchema = new mongoose.Schema({
+    instructorName: {
+        type: String,
+        required: true
+    },
+    subject: {
+        type: String,
+        required: true,
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    yearOfWriting: {
+        type: Number,
+        required:true
+    },
+    branch: {
+        type: String,
+        required: true
+    },
+    semester: {
+        type: Number,
+        required: true
+    },
+    materialType:{
+        type:String,
+        required:true
+    }
+}, { timestamp: true });
+
+
+export default mongoose.model("Material", materialSchema);
