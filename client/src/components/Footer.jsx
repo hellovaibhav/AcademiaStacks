4f9f7d75f76a8dcbaa523b1bbcac203fdd4da714
Saquib1973 <saquib.21ug2015@iiitranchi.ac.in>
2023-03-14 23:31:44 +0530

    added animation to about page,second update for about page

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-
+import {motion} from "framer-motion"
 const Footer = () => {
   return (
-    <div className="bg-black text-center text-white">
+    <motion.div className="overflow-hidden bg-blue-400 text-center text-white"
+    initial={{x:"100%"}}
+    whileInView={{x:"0%" , transition:{duration:0.5}}}>
       <h1>Made With Love</h1>
       <h1>@2023</h1>
-    </div>
+    </motion.div>
   );
 };
 
