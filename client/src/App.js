b18739d7b937fb036623ea99e74fe9348b7ea48d
Saquib1973 <saquib.21ug2015@iiitranchi.ac.in>
2023-03-14 23:31:44 +0530

    added animation to about page,second update for about page

--- client/src/App.js
+++ client/src/App.js
@@ -12,30 +12,30 @@ import { useEffect, useState } from "react";
 import NavbarLogin from "./components/NavbarLogin";
 import Register from "./pages/Register";
 function App() {
-  
-  const [isLoginPage, setIsLoginPage] = useState(false)
-  const locationName = useLocation()
-  console.log(locationName.pathname.split('/')[1])
+  const [isLoginPage, setIsLoginPage] = useState(false);
+  const locationName = useLocation();
+  console.log(locationName.pathname.split("/")[1]);
 
   useEffect(() => {
-    (locationName.pathname.split('/')[1] === "login" || locationName.pathname.split('/')[1] === "register") ? setIsLoginPage(true) : setIsLoginPage(false)
-  }, [locationName])
-  
+    locationName.pathname.split("/")[1] === "login" ||
+    locationName.pathname.split("/")[1] === "register"
+      ? setIsLoginPage(true)
+      : setIsLoginPage(false);
+  }, [locationName]);
+
   return (
-    <div className="App">
-    {
-      !isLoginPage ? <Navbar />: <NavbarLogin />
-    }
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/material" element={<Material />} />
-        <Route path="/feedback" element={<Feedback />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
-      <Footer />
+    <div className="App overflow-hidden">
+      {!isLoginPage ? <Navbar /> : <NavbarLogin />}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/material" element={<Material />} />
+          <Route path="/feedback" element={<Feedback />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+        <Footer />
     </div>
   );
 }
