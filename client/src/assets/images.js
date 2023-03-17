359178ee88e1a21ebd5e49c3e254488de5140004
Saquib1973 <saquib.21ug2015@iiitranchi.ac.in>
2023-03-18 00:52:34 +0530

    updated the navbar and favicon icon

--- client/src/assets/images.js
+++ client/src/assets/images.js
@@ -5,5 +5,5 @@ import image4 from "../assets/pic3.png"
 import image5 from "../assets/pic4.png"
 import image6 from "../assets/pic5.png"
 import image7 from "../assets/pic6.png"
-
-export default [image1 , image2 , image3 , image4 , image5 , image6 , image7]
+let image = [image1 , image2 , image3 , image4 , image5 , image6 , image7]
+export default image ; 
