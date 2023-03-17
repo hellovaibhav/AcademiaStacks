cc6154acf6c2e6daae3561367b71a2159fa956a5
Saquib1973 <saquib.21ug2015@iiitranchi.ac.in>
2023-03-17 18:30:06 +0530

    made about page completely responsive

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 const Footer = () => {
   return (
-    <div className="overflow-hidden bg-blue-400 text-center text-white">
+    <div className="overflow-hidden bg-blue-400 text-center text-white border-t-2 border-white">
       <h1>Made With Love</h1>
       <h1>@2023</h1>
     </div>
