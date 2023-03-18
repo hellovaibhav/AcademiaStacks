d62f9a91fe8bb341317c10f4a12e5ebc2708aa54
Saquib1973 <saquib.21ug2015@iiitranchi.ac.in>
2023-03-18 14:24:05 +0530

    added some hover effects , lottie elements to register and login page

--- client/src/components/AboutInfo.js
+++ client/src/components/AboutInfo.js
@@ -1,35 +1,34 @@
-import img1 from "../assets/pic3.png"
 let About = [
   {
-    image: img1,
+    image: 'https://avatars.githubusercontent.com/u/106366280?v=4',
     name: "Saquib Ali",
     position: "Dev",
     content:
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, quo. Natus dolor, illo sunt ut tenetur praesentium aut quam, officiis commodi nam facilis accusantium eum fuga provident iusto hic dolorum.",
   },
   {
-    image: img1,
+    image: 'https://avatars.githubusercontent.com/u/82583704?v=4',
     name: "Vaibhav Verma",
     position: "Dev",
     content:
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, quo. Natus dolor, illo sunt ut tenetur praesentium aut quam, officiis commodi nam facilis accusantium eum fuga provident iusto hic dolorum.",
   },
   {
-    image: img1,
+    image: 'https://avatars.githubusercontent.com/u/106348929?v=4',
     name: "Nishant Kumar",
     position: "Dev",
     content:
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, quo. Natus dolor, illo sunt ut tenetur praesentium aut quam, officiis commodi nam facilis accusantium eum fuga provident iusto hic dolorum.",
   },
   {
-    image: img1,
+    image: 'https://avatars.githubusercontent.com/u/96737391?v=4',
     name: "Vivek Giri",
     position: "Dev",
     content:
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, quo. Natus dolor, illo sunt ut tenetur praesentium aut quam, officiis commodi nam facilis accusantium eum fuga provident iusto hic dolorum.",
   },
   {
-    image: img1,
+    image: null,
     name: "Notes Provider",
     position: "Dev",
     content:
--- client/src/components/NavbarHead.jsx
+++ client/src/components/NavbarHead.jsx
@@ -2,26 +2,28 @@ import React, { useState } from "react";
 import { NavLink, Link } from "react-router-dom";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 import Logo from "../assets/logo_website.png";
+import {motion} from "framer-motion"
 const NavbarHead = () => {
+  const [registered, setRegistered] = useState(true)
   let Links = [
-    { name: "Home", link: "/" },
-    { name: "About", link: "/about" },
-    { name: "Material", link: "/material" },
-    { name: "Feedback", link: "/feedback" },
-    { name: "Login", link: "/login" },
+    { name: "Home",logo: "Home", link: "/" },
+    { name: "About",logo: "About", link: "/about" },
+    { name: "Material",logo: "Material", link: "/material" },
+    { name: "Feedback", logo: "Feedback",link: "/feedback" },
+    { name: "Login" , logo:`${registered ? 'Login':'Register'}` , link: `${registered ? '/login':'/register'}` },
   ];
   const [open, setOpen] = useState(false);
   return (
     <div className="z-10 drop-shadow-sm w-full fixed top-0 left-0">
-      <div className="md:flex md:items-center justify-between bg-white py-4 md:py-0 md:pl-10 md:pr-0 px-7">
+      <div className="md:flex md:items-center justify-between bg-white md:bg-transparent pt-4 md:h-auto h-24 md:py-0 md:pl-10 md:pr-0 px-7">
         <Link
           to="/"
           onClick={() => {
             setOpen(!open);
           }}
-          className="text-3xl inline text-indigo-600 mr-1 pt-2 font-bold cursor-pointer md:flex items-center font-[Poppins]"
+          className="text-3xl inline text-indigo-600 mr-1 font-bold cursor-pointer md:flex items-center font-[Poppins]"
         >
-          <img src={Logo} className="w-14" alt="LOGO" />
+          <img src={Logo} className="w-14 drop-shadow-md" alt="LOGO" />
         </Link>
         {open ? (
           <AiOutlineClose
@@ -34,6 +36,7 @@ const NavbarHead = () => {
           <AiOutlineMenu
             onClick={() => {
               setOpen(!open);
+
             }}
             className="text-3xl absolute right-4 top-8 z-20 cursor-pointer md:hidden inline"
           />
@@ -42,13 +45,15 @@ const NavbarHead = () => {
         <ul
           className={` ${
             open ? "top-24" : "top-[-490px]"
-          } bg-white  md:bg-blue-400 md:pl-16 pl-0 md:rounded-bl-full rounded-none h-auto md:h-16 md:flex md:items-center md:pb-0 pb-12 absolute md:static  w-full left-0 md:w-auto transition-all duration-500 ease-in`}
+          } bg-white  md:bg-blue-400 drop-shadow-lg md:pl-16 pl-0 md:rounded-bl-full rounded-none h-auto md:h-16 md:flex md:items-center absolute md:static  w-full left-0 md:w-auto transition-all duration-500 ease-in`}
         >
           {Links.map((link) => (
-            <li
+            <motion.li
               key={link.name}
               className="text-blue-400 hover:text-white md:text-white md:hover:text-blue-400 md:ml-8 mx-4 text-xl md:my-0 my-7 "
-            >
+              whileTap={{ scale: 0.9, transition: { duration: 0.1 } }}
+            
+              >
               {" "}
               <NavLink
                 onClick={() => {
@@ -57,9 +62,9 @@ const NavbarHead = () => {
                 to={link.link}
                 className="text-2xl font-light px-4 md:hover:text-bg-blue-400  md:hover:bg-white hover:bg-blue-400 hover:rounded-xl hover:py-[0.65rem] transition-all duration-200 ease-in"
               >
-                {link.name}
+                {link.logo}
               </NavLink>
-            </li>
+            </motion.li>
           ))}
         </ul>
       </div>
