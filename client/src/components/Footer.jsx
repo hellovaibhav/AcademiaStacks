f85db0034ef504c0ef5194d1167b969cadd053f9
Vaibhav <vermavaibhav2005@gmail.com>
2023-03-19 09:26:45 +0530

    linked the frontend register page with backend code and added useFetch hook

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+
+const dt = new Date();
+
 const Footer = () => {
   return (
     <div className="overflow-hidden bg-blue-400 text-center text-white border-t-2 border-white">
-      <h1>Made With Love</h1>
-      <h1>@2023</h1>
+      <h1>Made With 💖 by Hacktivators+</h1>
+      <h1>© {dt.getFullYear()}</h1>
     </div>
   );
 };
