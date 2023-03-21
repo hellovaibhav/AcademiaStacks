c9c81a9aa6a7d2fb7724d342b812b80683aa311b
Saquib1973 <saquib.21ug2015@iiitranchi.ac.in>
2023-03-21 20:25:01 +0530

    added links to the material tile

--- client/src/components/NavbarHead.jsx
+++ client/src/components/NavbarHead.jsx
@@ -3,57 +3,62 @@ import { NavLink, Link } from "react-router-dom";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 import { AuthContext } from "../context/AuthContext";
 import Logo from "../assets/logo_website.png";
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 const NavbarHead = () => {
   const { user } = useContext(AuthContext);
-  const [registered, setRegistered] = useState(true)
+  const [registered, setRegistered] = useState(true);
   let Links = [
     { name: "Home", logo: "Home", link: "/" },
     { name: "About", logo: "About", link: "/about" },
     { name: "Material", logo: "Material", link: "/material" },
     { name: "Feedback", logo: "Feedback", link: "/feedback" },
-    { name: "Login", logo: `${user ? "Hello " + user.fname : 'Login'}`, link: `${registered ? '/login' : '/register'}` },
+    {
+      name: "Login",
+      logo: `${user ? "Hello " + user.fname : "Login"}`,
+      link: `${registered ? "/login" : "/register"}`,
+    },
   ];
   const [open, setOpen] = useState(false);
   return (
     <div className="z-10 drop-shadow-lg md:drop-shadow-sm w-full fixed top-0 left-0">
       <div className="md:flex md:items-center justify-between bg-white md:bg-transparent pt-4 md:h-auto h-24 md:py-0 md:pl-10 md:pr-0 px-7">
-        <Link
-          to="/"
-          onClick={() => {
-            setOpen(!open);
-          }}
-          className="text-3xl inline text-indigo-600 mr-1 font-bold cursor-pointer md:flex items-center font-[Poppins]"
-        >
-          <img src={Logo} className="w-14 drop-shadow-md" alt="LOGO" />
-        </Link>
-        {open ? (
-          <AiOutlineClose
+        <div className="flex items-center justify-between">
+          <Link
+            to="/"
             onClick={() => {
-              setOpen(!open);
+              setOpen(false);
             }}
-            className="text-3xl absolute right-4 top-8 z-20 cursor-pointer md:hidden inline"
-          />
-        ) : (
-          <AiOutlineMenu
-            onClick={() => {
-              setOpen(!open);
-
-            }}
-            className="text-3xl absolute right-4 top-8 z-20 cursor-pointer md:hidden inline"
-          />
-        )}
+            className="text-3xl inline text-indigo-600 mr-1 font-bold cursor-pointer md:flex items-center font-[Poppins]"
+          >
+            <img src={Logo} className="w-14 drop-shadow-md" alt="LOGO" />
+          </Link>
+          {open ? (
+            <AiOutlineClose
+              onClick={() => {
+                setOpen(!open);
+              }}
+              className="text-3xl   z-20 cursor-pointer md:hidden inline"
+            />
+          ) : (
+            <AiOutlineMenu
+              onClick={() => {
+                setOpen(!open);
+              }}
+              className="text-3xl   z-20 cursor-pointer md:hidden inline"
+            />
+          )}
+        </div>
 
         <ul
-          className={` ${open ? "top-24" : "top-[-490px]"
-            } bg-white  md:bg-blue-400 drop-shadow-lg md:pl-16 pl-0 md:rounded-bl-full rounded-none h-auto md:h-16 md:flex md:items-center absolute md:static  w-full left-0 md:w-auto transition-all duration-500 ease-in`}
+          className={` ${
+            open ? "top-24" : "top-[-490px]"
+          } bg-white  md:bg-blue-400 drop-shadow-lg md:pl-16 pl-0 md:rounded-bl-full rounded-none h-auto md:h-16 md:flex md:items-center absolute md:static  w-full left-0 md:w-auto transition-all duration-500 ease-in`}
         >
           {Links.map((link) => (
             <motion.li
               key={link.name}
               className="text-blue-400 hover:text-white md:text-white md:hover:text-blue-400 md:ml-8 mx-4 text-xl md:my-0 my-7 "
               whileTap={{ scale: 0.9, transition: { duration: 0.1 } }}
-
             >
               {" "}
               <NavLink
