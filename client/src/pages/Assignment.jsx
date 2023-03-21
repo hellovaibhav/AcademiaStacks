6150d28faf8642ead1dd2cae09bc36e0b3d873dc
Saquib1973 <saquib.21ug2015@iiitranchi.ac.in>
2023-03-21 17:27:49 +0530

    Updating Responsive Design

--- client/src/pages/Assignment.jsx
+++ client/src/pages/Assignment.jsx
@@ -1,10 +1,13 @@
 import { motion } from "framer-motion";
-import React,{useState} from "react";
+import React, { useState, useEffect } from "react";
 // import img from "../assets/pic1.png";
 import MaterialPage from "../assets/MaterialPageContent";
+import axios from "axios";
 
 const Assignment = () => {
   const [index, setIndex] = useState(7);
+  const [data, setData] = useState([]);
+
   const showMore = () => {
     if (index < MaterialPage.length - 1) {
       if (index + 4 > MaterialPage.length - 1) {
@@ -15,7 +18,16 @@ const Assignment = () => {
       setIndex(index + 4);
     }
   };
-
+  const fetchNotes = async () => {
+    const { data } = await axios.get(
+      "http://localhost:8800/api/materials/Notes"
+    );
+    console.log(data);
+    setData(data);
+  };
+  useEffect(() => {
+    fetchNotes();
+  }, []);
   return (
     <div className="min-h-[100vh]  flex items-center justify-around mt-16 md:mt-24 pl-0 sm:pl-[2rem] lg:pl-[25rem] md:pl-[10rem] xl:pl-[30rem] ">
       <div className="leftFilter fixed top-52 left-14 h-40 hidden lg:block border-4 border-black w-[20vw]">
@@ -24,23 +36,48 @@ const Assignment = () => {
       <div className="flex flex-col items-center justify-around pb-10">
         <div className="rightContent w-[80vw] flex flex-wrap mt-14 lg:mt-0">
           {MaterialPage.slice(0, index + 1).map((material) => (
-            <div className="parent flex flex-col h-60 w-[100vw] md:w-56 bg-red-100 m-8 rounded-xl drop-shadow-md">
+            <motion.div
+              className="parent flex flex-col h-60 w-[100vw] md:w-56 bg-red-100 m-8 rounded-xl drop-shadow-md"
+              whileTap={{
+                scale: 0.9,
+                transition: { duration: 0.5 },
+              }}
+              initial={{ y: 150, opacity: 0 }}
+              animate={{
+                y: 0,
+                opacity: 1,
+                transition: { duration: 0.4, delay: 0.4 },
+              }}
+            >
               <img
-                src={material.images}
+                src={material.image}
                 alt="Not available"
                 className="h-[12rem] w-auto"
               />
-              <div className="descriptionChild p-2">{material.description}</div>
-            </div>
+              <div className="descriptionChild p-2">{material.subject}</div>
+            </motion.div>
           ))}
         </div>
-        {index < MaterialPage.length - 1 && (
+        {index < MaterialPage.length - 1 ? (
           <motion.button
             whileTap={{ scale: 0.7, transition: { duration: 0.1 } }}
             onClick={showMore}
             className="h-10 bg-blue-400 p-2 rounded-lg drop-shadow-md hover:bg-blue-200 mb-4 2xl:mr-10 xl:mr-[15rem]  "
           >
             Show More
+          </motion.button>
+        ) : (
+          <motion.button
+            whileTap={{
+              x: [0, 10, 0, -10, 0],
+              backgroundColor: "#ff3333",
+              color: "white",
+              transition: { duration: 0.1 },
+            }}
+            onClick={null}
+            className="h-10 bg-rose-400 p-2 rounded-lg drop-shadow-md  mb-4 2xl:mr-10 xl:mr-[15rem]  "
+          >
+            Thank You See You Later
           </motion.button>
         )}
       </div>
--- client/src/pages/PYQ.jsx
+++ client/src/pages/PYQ.jsx
@@ -1,21 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 // import img from "../assets/pic1.png";
-import MaterialPage from "../assets/MaterialPageContent";
+// import MaterialPage from "../assets/MaterialPageContent";
+import axios from "axios";
+
 
 const PYQ = () => {
   const [index, setIndex] = useState(7);
+  const [data, setData] = useState([]);
+
   const showMore = () => {
-    if (index < MaterialPage.length - 1) {
-      if (index + 4 > MaterialPage.length - 1) {
-        setIndex(MaterialPage.length);
+    if (index < data.length - 1) {
+      if (index + 4 > data.length - 1) {
+        setIndex(data.length);
       } else {
         setIndex(index + 4);
       }
       setIndex(index + 4);
     }
   };
-
+  const fetchNotes = async () => {
+    const { data } = await axios.get(
+      "http://localhost:8800/api/materials/Notes"
+    );
+    console.log(data);
+    setData(data);
+  };
+  useEffect(() => {
+    fetchNotes();
+  }, []);
   return (
     <div className="min-h-[100vh]  flex items-center justify-around mt-16 md:mt-24 pl-0 sm:pl-[2rem] lg:pl-[25rem] md:pl-[10rem] xl:pl-[30rem] ">
       <div className="leftFilter fixed top-52 left-14 h-40 hidden lg:block border-4 border-black w-[20vw]">
@@ -23,18 +36,18 @@ const PYQ = () => {
       </div>
       <div className="flex flex-col items-center justify-around pb-2">
         <div className="rightContent w-[80vw] flex flex-wrap mt-14 lg:mt-0">
-          {MaterialPage.slice(0, index + 1).map((material) => (
+          {data.slice(0, index + 1).map((material) => (
             <div className="parent flex flex-col h-60 w-[100vw] md:w-56 bg-red-100 m-8 rounded-xl drop-shadow-md">
               <img
                 src={material.images}
                 alt="Not available"
                 className="h-[12rem] w-auto"
               />
-              <div className="descriptionChild p-2">{material.description}</div>
+              <div className="descriptionChild p-2">{material.subject}</div>
             </div>
           ))}
         </div>
-        {(index < MaterialPage.length - 1) ? (
+        {index < data.length - 1 ? (
           <motion.button
             whileTap={{ scale: 0.7, transition: { duration: 0.1 } }}
             onClick={showMore}
@@ -42,15 +55,20 @@ const PYQ = () => {
           >
             Show More
           </motion.button>
-        ):
-        <motion.button
-        whileTap={{ x:[0,10,0,-10,0] , backgroundColor:"#ff3333" ,color:"white", transition: { duration: 0.1 } }}
-        onClick={null}
-        className="h-10 bg-rose-400 p-2 rounded-lg drop-shadow-md  mb-4 2xl:mr-10 xl:mr-[15rem]  "
-      >
-        Thank You See You Later
-      </motion.button>
-      }
+        ) : (
+          <motion.button
+            whileTap={{
+              x: [0, 10, 0, -10, 0],
+              backgroundColor: "#ff3333",
+              color: "white",
+              transition: { duration: 0.1 },
+            }}
+            onClick={null}
+            className="h-10 bg-rose-400 p-2 rounded-lg drop-shadow-md  mb-4 2xl:mr-10 xl:mr-[15rem]  "
+          >
+            Thank You See You Later
+          </motion.button>
+        )}
       </div>
     </div>
   );
