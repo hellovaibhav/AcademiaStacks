90b4bf7af4f6ad01c24873deb8850fc13e32ec20
Saquib1973 <saquib.21ug2015@iiitranchi.ac.in>
2023-03-29 00:36:15 +0530

    updated upvote links

--- client/src/pages/PYQ.jsx
+++ client/src/pages/PYQ.jsx
@@ -185,7 +185,7 @@ const Notes = () => {
                             let materialId = material._id;
                             try {
                               const res = await axios.post(
-                                "http://localhost:8800/api/materials/upvote",
+                                "https://academia-stacks.vercel.app/api/materials/upvote",
                                 { materialId, email }
                               );
                             } catch (err) {
