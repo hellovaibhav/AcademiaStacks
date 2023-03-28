2a6bd73e913b915e4d97ec2ca06f16bac278cb93
Vaibhav <vermavaibhav2005@gmail.com>
2023-03-29 00:07:25 +0530

    modified the origin

--- api/index.js
+++ api/index.js
@@ -17,7 +17,7 @@ const app = express();
 const port = process.env.PORT || 8800;
 
 const options = {
-    origin : 'http://localhost:3000',
+    origin : 'https://academia-stacks.vercel.app',
     useSuccessStatus : true
 }
 app.use(cors(options));
