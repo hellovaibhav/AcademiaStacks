4d5742fe9ce0357301bcd7b7860e9683453f81a2
Vaibhav <vermavaibhav2005@gmail.com>
2023-03-28 20:57:42 +0530

    added files required for making api files  vercel compatible

---
+++ api/routes/home.js
@@ -0,0 +1,10 @@
+import express from "express";
+import cors from "cors";
+
+const router = express.Router();
+
+router.get("/", cors(), (req, res) => {
+    res.status(200).json("The app is up and running");
+});
+
+export default router
