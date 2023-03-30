207cf50b0a59e327edc67d67d39fa3bfd69dd67d
Vaibhav <vermavaibhav2005@gmail.com>
2023-03-30 16:58:05 +0530

    resolved issue #5

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -29,6 +29,10 @@ export const register = async (req, res, next) => {
     var str = req.body.email;
     var name = str.split("@")[0];
 
+    var domain =str.split("@")[1];
+
+    var username = name + domain.split(".")[0];
+
     var naming = req.body.name;
     var firstname = naming.split(" ")[0];
 
@@ -37,7 +41,7 @@ export const register = async (req, res, next) => {
     const newUser = new User({
       name: req.body.name,
       email: req.body.email,
-      username: name,
+      username: username,
       password: hash,
       branch: req.body.branch,
       batch: req.body.batch,
@@ -51,6 +55,7 @@ export const register = async (req, res, next) => {
       from: senderMail,
       to: req.body.email,
       subject: "Academia Stacks One Time Password ",
+      text:"Your OTP for Academia Stacks registration is "+otpCode,
       html: "<html><body align=\"center\" bgcolor=\"#EDF1D6\"><p>We are happy to see that you want to join Academia Stacks and dig deep into the ocean of knowledge</p><br><h3> Here is your OTP </h3><br><h1>" + otpCode + " </h1><br><p>Please enter this within 30-Minutes to activate your account.</p> <br><br><p align=\"left\"> This is a system generated email. Please do not reply to this message. </p> <br><br><div align=\"left\"><h4>Academia Stacks</h4><h5>Verification Mail | Hacktivators<br>hacktivators.iiit@gmail.com<br></h5><h6>IIIT Ranchi, JUPMI Campus, Ranchi, Jharkhand</h6></div></body></html>"
     };
 
