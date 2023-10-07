57d6355089fb6b76655064997914474ceb4d42fc
Vaibhav <82583704+hellovaibhav@users.noreply.github.com>
2023-10-07 11:50:31 +0530

    added 2023 field in register page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -114,6 +114,7 @@ const Register = () => {
                 <option value="" disabled defaultValue>
                   Select Batch
                 </option>
+                <option value="2023">2023</option>
                 <option value="2022">2022</option>
                 <option value="2021">2021</option>
                 <option value="2020">2020</option>
